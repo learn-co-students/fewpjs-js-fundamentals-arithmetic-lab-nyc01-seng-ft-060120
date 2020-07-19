e9c8bdaa0b26181f65613d644bf4f0328ad71112
shiro-han <shan99business@gmail.com>
2020-07-19 15:53:02 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-
-// Write your code here
+let num1 = 31
+let num2 = 2
+let array = [1, 2, 3, 20]
+let multiply = num1 * num2;
+let random = Math.floor(Math.random() * 10) + 1;
+let mod = 9%5
+let max = Math.max(...array)
