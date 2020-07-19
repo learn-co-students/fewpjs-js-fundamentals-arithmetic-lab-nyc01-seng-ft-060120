6327d4164073bb78ca0cbbee6fce0dcc53ef2dc3
aaltmark <adamaltmark@gmail.com>
2020-07-19 19:19:45 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+//multiply vars num1 and num2 will equal 62 
+let num1 = 2 
+let num2 = 31
+let multiply = num1 * num2 
+
+//generate random integer greater than 0 
+let random = 10 * Math.random(); 
+
+//set to an equation whose remainder is 4 
+let mod = 19 % 5 
+
+//return 20 as the highest number in the set 
+let max = Math.max(1, 2, 20);
