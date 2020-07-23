e638a10bd8432fd656bbf888330fd0a7e1156490
Bearinawolfpack <bearinawolfpack@gmail.com>
2020-07-23 15:24:15 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,10 @@
+
+const num1 = 31
+const num2 = 2
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 10 + 1);
+
+const mod = 24 % 5;
+
+const max = Math.max(4, 8, 12, 16, 20);
