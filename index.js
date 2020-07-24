e8c4daa25c33f1085c9bd617b0743e02a547a3d3
JeanmarieJackman <jeanmarie.jackman@gmail.com>
2020-07-24 11:54:36 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
+// Assignment
+// There are four challenges we need you to solve. Code your solution in index.js. We'll provide some brief instructions here, but you should really rely on the test failure messages to guide your code.
+
+// Instructions
+// Create a variable called multiply that will multiply the variables num1 and num2 
+    //whose results will equal "62".
+// Create a variable called random that will generate a random integer greater than 0.
+// Create a variable called mod set to an equation whose remainder is equal to "4".
+// Create a variable called max that will return "20" as the highest number in the set.
 
 // Write your code here
+
+let num1 = 31
+let num2 = 2
+const multiply = num1 * num2
+console.log(multiply)
+
+const random = Math.random()
+console.log(random)
+
+const mod = 19 % 5
+console.log(mod)
+
+const max = Math.max(20, 4, 5, 6)
+console.log(max)
